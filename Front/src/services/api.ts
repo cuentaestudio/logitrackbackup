134be66a40541d69a1e86001d97ef4581937fb0a
cuentaestudio <cuenta.estudio1124@gmail.com>
2026-04-05 21:43:25 -0300

    Update api.ts

--- Front/src/services/api.ts
+++ Front/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const DEFAULT_API_BASE_URL = 'https://logitrack-api-4d2k.onrender.com/api'
+const DEFAULT_API_BASE_URL = 'https://logitrackbackup.onrender.com/api'
 
 const API_BASE_URL = (import.meta.env.VITE_API_BASE_URL || DEFAULT_API_BASE_URL).replace(/\/$/, '')
 
